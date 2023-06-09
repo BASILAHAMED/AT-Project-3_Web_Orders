cfb3589f0c2e84f92466332a97966fc7d62b46e2
BASILAHAMED <sbasil.ahamed@gmail.com>
2023-06-09 17:28:09 +0530

    login logout tested

---
+++ Test_Codes/test_login.py
@@ -0,0 +1,64 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from Test_Data import login_data
+import pytest
+
+
+class TestLogin:
+    url = "http://secure.smartbearsoftware.com/samples/TestComplete11/WebOrders/Login.aspx"
+    
+    # Launching driver for running the Python Tests
+    @pytest.fixture
+    def launch_driver(self):
+        self.driver = webdriver.Chrome()
+        self.driver.maximize_window()
+        self.driver.implicitly_wait(8)
+        yield
+        self.driver.close()
+    
+    def test_login1(self, launch_driver):
+        self.driver.get(self.url)
+        self.driver.find_element(by=By.XPATH, value=login_data.ElementLocators.xpath_username).send_keys(login_data.LoginData.username1)
+        self.driver.find_element(by=By.XPATH, value=login_data.ElementLocators.xpath_password).send_keys(login_data.LoginData.password1)
+        self.driver.find_element(by=By.XPATH, value=login_data.ElementLocators.xpath_login).click()
+        warning = self.driver.find_element(by=By.XPATH, value=login_data.ElementLocators.xpath_invalid_login).text
+        assert warning == 'Invalid Login or Password.'
+        print("SUCCESS # INVALID LOGIN WITH USERNAME {username} and PASSWORD {password}".format(username=login_data.LoginData.username1, password=login_data.LoginData.password1))
+
+    def test_login2(self, launch_driver):
+        self.driver.get(self.url)
+        self.driver.find_element(by=By.XPATH, value=login_data.ElementLocators.xpath_username).send_keys(login_data.LoginData.username2)
+        self.driver.find_element(by=By.XPATH, value=login_data.ElementLocators.xpath_password).send_keys(login_data.LoginData.password2)
+        self.driver.find_element(by=By.XPATH, value=login_data.ElementLocators.xpath_login).click()
+        warning = self.driver.find_element(by=By.XPATH, value=login_data.ElementLocators.xpath_invalid_login).text
+        assert warning == 'Invalid Login or Password.'
+        print("SUCCESS # INVALID LOGIN WITH USERNAME {username} and PASSWORD {password}".format(username=login_data.LoginData.username3, password=login_data.LoginData.password2))
+              
+    def test_login3(self, launch_driver):
+        self.driver.get(self.url)
+        self.driver.find_element(by=By.XPATH, value=login_data.ElementLocators.xpath_username).send_keys(login_data.LoginData.username3)
+        self.driver.find_element(by=By.XPATH, value=login_data.ElementLocators.xpath_password).send_keys(login_data.LoginData.password3)
+        self.driver.find_element(by=By.XPATH, value=login_data.ElementLocators.xpath_login).click()
+        warning = self.driver.find_element(by=By.XPATH, value=login_data.ElementLocators.xpath_invalid_login).text
+        assert warning == 'Invalid Login or Password.'
+        print("SUCCESS # INVALID LOGIN WITH USERNAME {username} and PASSWORD {password}".format(username=login_data.LoginData.username3, password=login_data.LoginData.password3))
+
+    def test_login4(self, launch_driver):
+        self.driver.get(self.url)
+        self.driver.find_element(by=By.XPATH, value=login_data.ElementLocators.xpath_username).send_keys(login_data.LoginData.username4)
+        self.driver.find_element(by=By.XPATH, value=login_data.ElementLocators.xpath_password).send_keys(login_data.LoginData.password4)
+        self.driver.find_element(by=By.XPATH, value=login_data.ElementLocators.xpath_login).click()
+        info = self.driver.find_element(by=By.XPATH, value=login_data.ElementLocators.xpath_login_info).text
+        assert info == 'Welcome, Tester! | Logout'
+        print("SUCCESS # LOGGED IN WITH USERNAME {username} and PASSWORD {password}".format(username=login_data.LoginData.username4, password=login_data.LoginData.password4))
+
+    def test_logout(self, launch_driver):
+        self.driver.get(self.url)
+        self.driver.find_element(by=By.XPATH, value=login_data.ElementLocators.xpath_username).send_keys(login_data.LoginData.username4)
+        self.driver.find_element(by=By.XPATH, value=login_data.ElementLocators.xpath_password).send_keys(login_data.LoginData.password4)
+        self.driver.find_element(by=By.XPATH, value=login_data.ElementLocators.xpath_login).click()
+        self.driver.implicitly_wait(4)
+        self.driver.find_element(by=By.XPATH, value=login_data.ElementLocators.xpath_logout).click()
+        username_box = self.driver.find_element(by=By.XPATH, value=login_data.ElementLocators.xpath_username_box).text
+        assert username_box == 'Username:'
+        print("SUCCESS # LOGGED OUT")
---
+++ Test_Data/login_data.py
@@ -0,0 +1,23 @@
+class LoginData:
+    # combo1
+    username1 = 'Tester'
+    password1 = 'password'
+    # combo2
+    username2 = 'username'
+    password2 = 'test'
+    # combo3
+    username3 = 'username'
+    password3 = 'password'
+    # combo4
+    username4 = 'Tester'
+    password4 = 'test'
+
+class ElementLocators:
+    xpath_username = '//input[@id="ctl00_MainContent_username"]'
+    xpath_password = '//input[@id="ctl00_MainContent_password"]'
+    xpath_login = '//input[@id="ctl00_MainContent_login_button"]'
+    xpath_invalid_login = '//span[@id="ctl00_MainContent_status"]'
+    xpath_login_info = '//div[@class="login_info"]'
+    xpath_username_box = '//label[@for="ctl00_MainContent_username"]'
+    xpath_logout = '//a[@id="ctl00_logout"]'
+
